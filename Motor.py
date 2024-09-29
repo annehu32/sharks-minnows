ac94fdb07abb9872a0516c22262a0ce7e3f7dbd4
Anne Hu <anne.hu@tufts.edu>
2024-09-28 21:20:51 -0400

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -3,34 +3,46 @@ import network
 from machine import Pin, PWM
 
 class Motor():
+    
+    #Constants to be fiddled with
+    pwmMin = 25
+    pwmMax = 255
 
     # Initializer takes pins for the neopixel and buzzer
-    def __init__(self, pin1, pin2, name):
-        self.forward = pin1
-        self.back = pin2
+    def __init__(self, pin1, pin2, pwm, name):
+        self.forward = pin1 #Should be PWM
+        self.back = pin2 #Should be PWM
+        self.pwm = pwm
         self.name = name
         print('motor instantiated, name: '+self.name)
+        
+        self.pwm.freq(1000)
     
-    def goForward(self):
-        print(self.name + ' motor moving forward')
+    def goForward(self, val):
+        print(self.name + ' moving forward')
         self.back.off()
         time.sleep(0.01)
         self.forward.on()
+        self.pwm.duty_u16(int(val*65535/100))
     
-    def goBackward(self):
-        print(self.name + ' motor moving backward')
+    def goBackward(self, val):
+        print(self.name + ' moving backward')
         self.forward.off()
         time.sleep(0.01)
         self.back.on()
-        
+        self.pwm.duty_u16(int(val*65535/100)) # where val is a %
+
     def stop(self):
-        print(self.name + ' motor stopping ')
+        print(self.name + ' stopping ')
         self.forward.off()
         self.back.off()
     
     def test(self):
-        self.goForward()
-        time.sleep(1)
-        self.goBackward()
-        time.sleep(1)
+        for i in range(25,101):
+            self.goForward(i)
+            print(i)
+            time.sleep(0.05)
+        for i in range(25, 101):
+            self.goBackward(i)
+            time.sleep(0.05)
         self.stop()
